5667eeb18d71936e1f93a5bd5674ca8e5eab7df7
Mario Riascos <mariofernando07@hotmail.com>
2016-08-05 17:13:54 -0500

    api entidades creada v2"

--- app.js
+++ app.js
@@ -22,8 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next){
 
   // Website you wish to allow to connect
-  //res.setHeader('Access-Control-Allow-Origin', '*');
-  //req.setHeader('Access-Control-Allow-Origin', '*');    
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   req.db = mDB;
   next();
 });
